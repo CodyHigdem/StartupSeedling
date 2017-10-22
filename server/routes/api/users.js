cc48e813521b34118afa0fa65381e55ee71c2617
Cody <meanderingmonarch@gmail.com>
2017-10-22 09:08:44 -0500

    user registration route created

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -4,4 +4,22 @@ const passport = require('passport');
 const User = mongoose.model('User');
 const auth = require('../auth');
 
+/*
+* user registers
+* POST username, email, password
+*/
+router.post('/users', function(req, res, next){
+	const user = new User();
+
+	user.username = req.body.user.username;
+	user.email = req.body.user.email;
+	user.setPassword(req.body.user.password);
+
+	user.save().then(function(){
+		return res.json({
+			user: user.toAuthJSON()
+		});
+	}).catch(next);
+});
+
 module.exports = router;
