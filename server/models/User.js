27221d5cfd43d7e4d488cb1056252a8958d2135e
Cody <meanderingmonarch@gmail.com>
2017-10-22 00:03:50 -0500

    generate JWT

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const secret = require('../config').secret;
+
+
 
 const usernameValidation = {
 	type: String, 
@@ -45,5 +49,21 @@ UserSchema.methods.setPassword = function(password){
 	this.salt = crypto.randomBytes(16).toString('hex');
 	this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
 };
+
+UserSchema.methods.generateJWT = function(){
+	const today = new Date();
+	const exp = new Date(today);
+	exp.setDate(today.getDate() + 60);
+
+	return jwt.sign({
+		id: this._id,
+		username: this.username,
+		exp: parseInt(exp.getTime()/1000),
+	}, secret);
+};
+
+
+
+
 
 mongoose.model('User', UserSchema);
